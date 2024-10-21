512282bb6160fe628ac72a3449e014ffed79fd3f
Sara Acuña <acunabenavidessaraelis@gmail.com>
2024-10-20 20:49:14 -0500

    crear cuenta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from './page/create-account/create-account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'tasks', component: ''},  // Ruta para la lista de tareas
+  { path: 'create-account', component: CreateAccountComponent },  // Ruta para crear una tarea
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },  // Redirige a la lista de tareas por defecto
+  { path: '**', redirectTo: '/tasks' }  // Cualquier ruta no definida redirige a la lista de tareas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
